df5f0b28bf625493263fcd0b7a6fb55989089a9c
ArnoldPires <arnold.pires92@gmail.com>
2023-10-02 20:12:39 -0400

    Just the setup

--- components/header.jsx
+++ components/header.jsx
@@ -1,14 +1,14 @@
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
+export default class header extends Component {
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <h1>Star Wars API</h1>
       </header>
       <button className="generate-button">Generate StarShips</button>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
